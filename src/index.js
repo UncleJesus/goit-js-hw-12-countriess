a66f41a8e8956d2c5975c4c66adf42c95c414dff
Омельянов Дима <domelyanov@hotmail.com>
2020-11-02 00:47:07 +0200

    c

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-import css from "./css/style.css";
-
+import './styles.css';
---
+++ src/itemsOfGalery.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    url: './images/product1.png',
+    alt: 'product1',
+  },
+  {
+    url: './images/product2.png',
+    alt: 'product2',
+  },
+  {
+    url: './images/product3.png',
+    alt: 'product3',
+  },
+  {
+    url: './images/product4.png',
+    alt: 'product4',
+  },
+  {
+    url: './images/product5.png',
+    alt: 'product5',
+  },
+  {
+    url: './images/product6.png',
+    alt: 'product6',
+  },
+  {
+    url: './images/product7.png',
+    alt: 'product7',
+  },
+  {
+    url: './images/product8.png',
+    alt: 'product8',
+  },
+];
