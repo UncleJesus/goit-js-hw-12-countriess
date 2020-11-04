3ce7b9089a17e9f042b020ae8c5ec2f9254a103f
Омельянов Дима <domelyanov@hotmail.com>
2020-11-04 11:54:35 +0200

    c

---
+++ src/js/script.js
@@ -0,0 +1,70 @@
+import API from './fetchCountries.js';
+
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+
+import countriesMarkup from '../templates/countries-markup.hbs';
+import counriesList from '../templates/countries-list.hbs';
+
+const debounce = require('lodash.debounce');
+
+const refs = {
+  searchInput: document.querySelector('.input-control'),
+  countriesContainer: document.querySelector('.js-card-container'),
+};
+
+refs.searchInput.addEventListener('input', debounce(onSearch, 500));
+
+function onSearch(e) {
+  let countrySearch = e.target.value;
+
+  if (!countrySearch) {
+    clearMarkup();
+    return;
+  }
+
+  API.fetchCountries(countrySearch).then(chooseCountry).catch(onError);
+}
+
+function chooseCountry(countries) {
+  if (countries.length > 10) {
+    clearMarkup();
+    tooManyMatchesError();
+  } else if (countries.length > 1 && countries.length <= 10) {
+    clearMarkup();
+    renderCountryMarkup(counriesList, countries);
+  } else if (countries.length === 1) {
+    clearMarkup();
+    renderCountryMarkup(countriesMarkup, countries);
+  } else {
+    noResults();
+  }
+}
+
+function renderCountryMarkup(option, countries) {
+  const markup = option(countries);
+  refs.countriesContainer.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearMarkup() {
+  refs.countriesContainer.innerHTML = '';
+}
+
+function onError() {
+  clearMarkup();
+}
+
+function tooManyMatchesError() {
+  error({
+    text: 'Too many matches found. Please enter a more specific query!',
+    delay: 1500,
+  });
+}
+
+function noResults() {
+  error({
+    text: 'No such country. Please enter another query!',
+    delay: 2000,
+  });
+}
